fbe46ead6627128f0ad05b65744cda32eaf8fb7e
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-23 21:47:06 -0700

    Lint

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const url = require("url");
-const path = require("path");
+const url = require('url');
+const path = require('path');
 
 module.exports = function parseUrl(sourceUrl) {
   const options = {};
-
   const urlParts = url.parse(sourceUrl, true);
-
-  options.dialect = urlParts.protocol.replace(/:$/, "");
-
+  options.dialect = urlParts.protocol.replace(/:$/, '');
   options.host = urlParts.hostname;
 
-  if (
-    options.dialect === "sqlite" &&
-    urlParts.pathname &&
-    !urlParts.pathname.startsWith("/:memory")
-  ) {
+  if (options.dialect === 'sqlite' && urlParts.pathname && !urlParts.pathname.startsWith('/:memory')) {
     const storagePath = path.join(options.host, urlParts.pathname);
-
     options.storage = path.resolve(options.storage || storagePath);
   }
 
   if (urlParts.pathname) {
-    options.database = urlParts.pathname.replace(/^\//, "");
+    options.database = urlParts.pathname.replace(/^\//, '');
   }
 
   if (urlParts.port) {
@@ -29,16 +21,13 @@ module.exports = function parseUrl(sourceUrl) {
   }
 
   if (urlParts.auth) {
-    const authParts = urlParts.auth.split(":");
-
+    const authParts = urlParts.auth.split(':');
     options.username = authParts[0];
-
-    if (authParts.length > 1) options.password = authParts.slice(1).join(":");
+    if (authParts.length > 1) options.password = authParts.slice(1).join(':');
   }
 
   if (urlParts.query) {
-    if (options.dialectOptions)
-      Object.assign(options.dialectOptions, urlParts.query);
+    if (options.dialectOptions) Object.assign(options.dialectOptions, urlParts.query);
     else options.dialectOptions = urlParts.query;
   }
 
