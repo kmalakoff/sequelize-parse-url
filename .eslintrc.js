e5c22a637cf809c59cca4eca31b37d8f11fb0603
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-02 00:00:39 -0700

    Refactor project structure and tooling configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,9 @@
 module.exports = {
+  extends: ['standard', 'prettier'],
+  parser: 'babel-eslint',
   env: {
-    browser: true,
-    commonjs: true,
     es6: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
+    node: true,
+    mocha: true,
   },
 };
---
+++ README.md
@@ -0,0 +1,12 @@
+# sequelize-parse-url
+
+Parses a sequelize url into a configuration object.
+
+Extracted from (sequelize)[https://github.com/sequelize/sequelize] codebase so you can use in your own workflows.
+
+```
+var assert = require('assert');
+var parse = require('sequelize-parse-url');
+
+assert.ok(env().NODE);
+```
